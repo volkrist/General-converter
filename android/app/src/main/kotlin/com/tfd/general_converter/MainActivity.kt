72132e058bcbc96882208d29dc2a5650b50a31dc
FrankMilvus <frankmilvus@gmail.com>
2026-04-24 01:10:25 +0300

    feat: implement system window insets handling in MainActivity

--- android/app/src/main/kotlin/com/tfd/general_converter/MainActivity.kt
+++ android/app/src/main/kotlin/com/tfd/general_converter/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.tfd.general_converter
 
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
+
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            val controller = window.insetsController
+            controller?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_BARS_BY_SWIPE
+            }
+        }
+    }
+}
